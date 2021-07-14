4c58c69953f7c6df36c9925b7b380f28d69d2cf5
Maximilian Törnqvist <maxnittiosex@gmail.com>
2021-07-14 02:30:59 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -78,9 +78,9 @@ class HighwayEnv(AbstractEnv):
 
     """def _reward(self, action: Action) -> float:
         """
-        The reward is defined to foster driving at high speed, on the rightmost lanes, and to avoid collisions.
+        """The reward is defined to foster driving at high speed, on the rightmost lanes, and to avoid collisions.
         :param action: the last action performed
-        :return: the corresponding reward
+        :return: the corresponding reward"""
         """
         neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
         lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
